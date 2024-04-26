3fea72286ba212ad790075c93dde87e72cfbe9bc
Justin Burrill <me@justinburrill.ca>
2024-04-26 19:34:57 -0400

    temp

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
+  <NavBar />
   <p>this is my text</p>
   <p>this is more of my text</p>
-  <!-- <HelloWorld /> -->
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
-  // components: {
-  //   HelloWorld
-  // }
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style>
+p {
+  color: black;
+}
+
 #app {
+  margin-top: 60px;
+}
+
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="bar">
+        <h1>justinburrill.ca</h1>
+        <nav>
+            <router-link to="/">Home</router-link>
+
+            <router-link to="/test">Test</router-link>
+        </nav>
+    </div>
+</template>
+
+<style lang="css">
+router-link {
+    color: black;
+}
+
+h1 {
+    font-weight: bold;
+    font-size: 40px;
+    margin: 10px;
+    padding: 10px;
+}
+
+nav {
+    text-align: right;
+}
+
+#bar {
+    position: fixed;
+    background-color: lightblue;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+
+}
+</style>
