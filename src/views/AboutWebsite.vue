43d7e2ec1e72171529043b88bda6329463a636c8
Justin Burrill <me@justinburrill.ca>
2024-10-04 16:06:57 -0400

    quick update

--- src/views/AboutWebsite.vue
+++ src/views/AboutWebsite.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="center">
     <p>This website hosted on a ThinkPad running Ubuntu sever 22.04</p>
-    <p>It stands on the shoulders of these technologies:</p>
+    <p>I used these technologies:</p>
     <div id="logo-div" class="flex-container">
       <div class="logo-box" id="vue-box">
         <img class="logo" src="../assets/vue-logo.png">
@@ -9,13 +9,16 @@
       </div>
       <div class="logo-box" id="apache-box">
         <img class="logo" src="../assets/apache-logo.png">
-        <p class="logo-label">Apache2</p>
+        <!-- <p class="logo-label">Apache2</p> -->
       </div>
       <div class="logo-box" id="sass-box">
         <img class="logo" src="../assets/sass-logo.png">
-        <p class="logo-label">Sass</p>
+        <!-- <p class="logo-label">Sass</p> -->
       </div>
-      <div class="logo-box">dunno yet</div>
+      <div class="logo-box" id="pnpm-box">
+        <img class="logo" src="../assets/pnpm-logo.png">
+
+      </div>
     </div>
 
   </div>
--- src/views/InteractiveDemos.vue
+++ src/views/InteractiveDemos.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="center">
         <p>This is a collection of programming projects that you can interact with on my website.</p>
+        <p>(There's nothing here yet...)</p>
     </div>
 </template>
