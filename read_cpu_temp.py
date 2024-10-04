4bb812789e29c4a8ddac0c6b7a85071855ea45d9
Justin Burrill <me@justinburrill.ca>
2024-10-04 15:36:17 -0400

    temperature script

---
+++ read_cpu_temp.py
@@ -0,0 +1,18 @@
+import subprocess
+
+
+def main():
+    #x = subprocess.run(["sensors"])
+    cpu_line = ""
+    with open("sensors.txt", "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.startswith("CPU:"):
+                cpu_line = line
+                break
+
+    cpu_line = cpu_line.removeprefix("CPU:").strip()
+    print(cpu_line)
+
+if __name__ == "__main__":
+    main()
