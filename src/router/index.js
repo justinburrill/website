8033fc539494408971f06e4cc4cbd697acdf5008
Justin Burrill <me@justinburrill.ca>
2024-10-24 22:02:39 -0400

    github redirect

--- src/router/index.js
+++ src/router/index.js
@@ -2,39 +2,47 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 
 const routes = [
-  {
-    path: '/:pathMatch(.*)*',
-    name: '404Page',
-    component: () => import("../views/404Page.vue")
-  },
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about-this-website',
-    name: 'about-this-website',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutWebsite.vue')
-  },
-  {
-    path: '/interactive-projects',
-    name: 'interactive-projects',
-    component: () => import("../views/InteractiveDemos.vue")
-  },
-  {
-    path: '/gradquote',
-    name: 'gradquote',
-    component: () => import("../views/GradQuote.vue")
-  }
+    {
+        path: '/:pathMatch(.*)*',
+        name: '404Page',
+        component: () => import("../views/404Page.vue")
+    },
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about-this-website',
+        name: 'about-this-website',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutWebsite.vue')
+    },
+    {
+        path: '/interactive-projects',
+        name: 'interactive-projects',
+        component: () => import("../views/InteractiveDemos.vue")
+    },
+    {
+        path: '/gradquote',
+        name: 'gradquote',
+        component: () => import("../views/GradQuote.vue")
+    },
+    {
+        path: '/github',
+        component: null,
+        name: 'github',
+        beforeEnter: () => {
+            window.location.href = "https://github.com/justinburrill";
+        }
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
