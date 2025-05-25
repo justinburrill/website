58c0cd972768112b0b914e41e3edb36f7f619674
Justin Burrill <me@justinburrill.ca>
2025-05-24 22:01:44 -0400

    fix start script

--- server/start.sh
+++ server/start.sh
@@ -2,6 +2,7 @@
 parent_path="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/.."
 
 # npm install sorta thing
+echo "--- Checking front-end dependencies... ---"
 [ -d "$parent_path/frontend/node_modules" ] && $parent_path/server/install.sh
 
 # build frontend
@@ -10,4 +11,4 @@ cd "$parent_path/frontend" && pnpm run build && cd - || exit
 
 # start it!
 echo "--- Starting deno server... ---"
-deno --allow-read --allow-net ./src/server.ts
+deno --allow-read --allow-net $parent_path/src/server.ts
